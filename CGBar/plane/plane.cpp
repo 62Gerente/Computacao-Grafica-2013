d22b5b8700ca6da615883aa81cb8ec23f4d47a99
André Santos <andreccdr@gmail.com>
2013-03-12 13:21:43 +0000

    Separado plano e plano rodado

--- CGBar/plane/plane.cpp
+++ CGBar/plane/plane.cpp
@@ -1,13 +1,7 @@
 #include <GLUT/glut.h>
 #include <math.h>
 
-void drawPlane(float width, float length, float anglex, float angley, float anglez){
-    glPushMatrix();
-    
-    glRotatef(anglex, 1, 0, 0);
-    glRotatef(angley, 0, 1, 0);
-    glRotatef(anglez, 0, 0, 1);
-    
+void drawPlane(float width, float length){    
     glBegin(GL_TRIANGLES);
     
     glVertex3f(width/2,0,length/2) ;
@@ -19,6 +13,17 @@ void drawPlane(float width, float length, float anglex, float angley, float angl
     glVertex3f(width/2,0,length/2) ;
     
     glEnd();
+}
+
+void drawPlaneRotated(float width, float length, float anglex, float angley, float anglez){
+    glPushMatrix();
+    
+    glRotatef(anglex, 1, 0, 0);
+    glRotatef(angley, 0, 1, 0);
+    glRotatef(anglez, 0, 0, 1);
+    
+    drawPlane(width, length);
+    
     glPopMatrix();
     
 }
