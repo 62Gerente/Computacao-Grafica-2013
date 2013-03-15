d51a51889284e2ba3eb2052d33ec764cca9762eb
André Santos <andreccdr@gmail.com>
2013-03-15 12:39:25 +0000

    Actualizado main e cilindro

--- CGBar/cylinder/cylinder.cpp
+++ CGBar/cylinder/cylinder.cpp
@@ -11,8 +11,8 @@ void drawCylinder(float radius, float height, int vertex){
     
     for(i=0;i<vertex;i++){
         glVertex3f(0,-(height/2),0);
-        glVertex3f(radius*sin(angact), -(height/2), radius*cos(angact));
         glVertex3f(radius*sin(angact + ang), -(height/2), radius*cos(angact + ang));
+        glVertex3f(radius*sin(angact), -(height/2), radius*cos(angact));
         
         glVertex3f(0,(height/2),0);
         glVertex3f(radius*sin(angact), (height/2), radius*cos(angact));
