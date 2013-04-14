796cfd4e0a07d0c7efce26a8fed3cb4b10afc9cd
helenalves4@gmail.com <lena4isa92>
2013-04-14 14:46:13 +0100

    Actualizado

---
+++ CGBar/objects/sconces/simpleSconce.h
@@ -0,0 +1,7 @@
+#ifndef _CAND_H_
+#define _CAND_H_
+
+
+void drawCandeeiro(double alt, int nlados, int ncamadas);
+
+#endif
---
+++ CGBar/objects/sconces/treeSconce.h
@@ -0,0 +1,7 @@
+#ifndef _CAND_3_H_
+#define _CAND_3_H_
+
+
+void drawCandeeiro_3(double alt, int nlados, int ncamadas);
+
+#endif
