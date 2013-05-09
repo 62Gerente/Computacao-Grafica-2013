2a7c962a3df7c35389107005fc6c713d972c51d7
André Santos <andreccdr@gmail.com>
2013-05-10 00:03:07 +0100

    Começada Estrutura

---
+++ CGBar/objects/estruturas/FloorVBO.h
@@ -0,0 +1,11 @@
+//#pragma once
+//class FloorVBO
+//{
+//private:
+//	double ratio;
+//	unsigned int id_textura ;
+//public:
+//	FloorVBO(double argRatio, unsigned int aId_textura);
+//	void draw();
+//};
+//
